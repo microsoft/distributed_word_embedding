6910a2709cbaa1234be57669d42bb8885c9f3e37
Guoqing Liu (MSR Student-Person Consulting) <v-guoql@microsoft.com>
2015-11-08 18:22:59 +0800

    input arguments format doc

---
+++ example/run.bat
@@ -0,0 +1,23 @@
+set size=300
+set text=enwiki2014
+set read_vocab="D:\Users\enwiki2014_vocab_m5.txt"
+set train_file="D:\Users\enwiki2014"
+set binary=1
+set cbow=1
+set alpha=0.01
+set epoch=20
+set window=5
+set sample=0
+set hs=0
+set negative=5
+set threads=16
+set mincount=5
+set sw_file=stopwords_simple.txt
+set stopwords=0
+set data_block_size=1000000000
+set max_preload_data_size=20000000000
+set use_adagrad=0
+set output=%text%_%size%.bin
+
+distributed_word_embedding.exe -max_preload_data_size %max_preload_data_size% -alpha %alpha% -data_block_size %data_block_size% -train_file %train_file% -output %output% -threads %threads% -size %size% -binary %binary% -cbow %cbow% -epoch %epoch% -negative %negative% -hs %hs% -sample %sample% -min_count %mincount% -window %window% -stopwords %stopwords% -sw_file %sw_file% -read_vocab %read_vocab% -use_adagrad %use_adagrad% 
+
