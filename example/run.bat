dbc51314b5b59dac36593bb5ab4636176f2ef1c5
fiberleif <v-guoql@microsoft.com>
2015-12-09 17:32:16 +0800

    Update run.bat
    run without pipeline

--- example/run.bat
+++ example/run.bat
@@ -17,7 +17,8 @@ set stopwords=0
 set data_block_size=1000000000
 set max_preload_data_size=20000000000
 set use_adagrad=0
+set is_pipeline=0
 set output=%text%_%size%.bin
 
-distributed_word_embedding.exe -max_preload_data_size %max_preload_data_size% -alpha %alpha% -data_block_size %data_block_size% -train_file %train_file% -output %output% -threads %threads% -size %size% -binary %binary% -cbow %cbow% -epoch %epoch% -negative %negative% -hs %hs% -sample %sample% -min_count %mincount% -window %window% -stopwords %stopwords% -sw_file %sw_file% -read_vocab %read_vocab% -use_adagrad %use_adagrad% 
+distributed_word_embedding.exe -max_preload_data_size %max_preload_data_size% -is_pipeline %is_pipeline% -alpha %alpha% -data_block_size %data_block_size% -train_file %train_file% -output %output% -threads %threads% -size %size% -binary %binary% -cbow %cbow% -epoch %epoch% -negative %negative% -hs %hs% -sample %sample% -min_count %mincount% -window %window% -stopwords %stopwords% -sw_file %sw_file% -read_vocab %read_vocab% -use_adagrad %use_adagrad% 
 
