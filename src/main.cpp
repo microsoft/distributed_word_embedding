e7adf7286e80dea2c802376ac4665701fce4ecf2
Aerosoul <chivee.ye@gmail.com>
2016-05-26 13:04:18 +0800

    Revert " update distributed word embedding"

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,42 @@
-﻿#include <cstring>
-#include <cmath>
-
 #include <thread>
 #include <string>
 #include <iostream>
+#include <cstring>
+#include <cmath>
 #include <vector>
 #include <fstream>
 #include <sstream>
 #include <new>
-//#include <vld.h> 
-
-#include "multiverso/util/log.h"
-#include "multiverso/multiverso.h"
-#include "distributed_wordembedding.h"
-#include "memory_manager.h"
 
 #include "dictionary.h"
 #include "huffman_encoder.h"
 #include "util.h"
 #include "reader.h"
+#include "multiverso.h"
+#include "barrier.h"
+#include "distributed_wordembedding.h"
+#include "parameter_loader.h"
+#include "trainer.h"
+#include "word_embedding.h"
+#include "memory_manager.h"
 
 using namespace multiverso;
 using namespace wordembedding;
-
 int main(int argc, char *argv[])
-{
-	try
-	{
-		Distributed_wordembedding *ptr = new (std::nothrow)Distributed_wordembedding();
-		assert(ptr != nullptr);
-		ptr->Run(argc, argv);
-		delete ptr;
-	}
-	catch (std::bad_alloc &memExp)
-	{
-		multiverso::Log::Info("Something wrong with new() %s\n", memExp.what());
-	}
-	catch (...)
-	{
-		multiverso::Log::Info("Something wrong with other reason!\n");
-	}
-	system("PAUSE");
-	return 0;
+{   
+    try
+    {
+        Distributed_wordembedding *ptr = new (std::nothrow)Distributed_wordembedding();
+        assert(ptr != nullptr);
+        ptr->Run(argc, argv);
+    }
+    catch (std::bad_alloc &memExp)
+    {
+        multiverso::Log::Info("Something wrong with new() %s\n", memExp.what());
+    }
+    catch(...)
+    {
+        multiverso::Log::Info("Something wrong with other reason!\n");
+    }
+    return 0;
 }
-
